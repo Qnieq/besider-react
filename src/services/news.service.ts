6dbad066ca1a3deb5f0411e4f5a19e1b101e4ba2
Mike <misha.o.son@gmail.com>
2025-03-05 19:48:42 +0300

    added env

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,14 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { NewsResponse } from '../types/news.types';
 
-const API_KEY = 'vtgVpvTm159wt4bU8lucT2zdBOB0GNL5';
-
 export const newsApi = createApi({
   reducerPath: 'newsApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   endpoints: (builder) => ({
     getNewsByMonth: builder.query<NewsResponse, { year: number; month: number }>({
-      query: ({ year, month }) => `${year}/${month}.json?api-key=${API_KEY}`,
+      query: ({ year, month }) => `${year}/${month}.json?api-key=${import.meta.env.VITE_API_KEY}`,
     })
   })
 });
